8eb1d87c33b06712bc2d9d7922cdc37c36f1414f
Samsung-Book <Sit-Samsung@vn.ru>
2016-02-16 13:36:34 +0500

    L&L UPDATE! now game have Lives & Levels -added lives -added levels -added new colors for bricks; colors are choosed randomly

--- ArkanoidGame/ArkMain.h
+++ ArkanoidGame/ArkMain.h
@@ -1,32 +1,18 @@
 #pragma once
 #ifndef ARKMAIN_H
-#define FRAME_VALUES 10
-#define MAX_MAP_X 10
-#define MAX_MAP_Y 15
-#define _CRT_SECURE_NO_WARNINGS
+#define ARKMAIN_H
 
 #include "SDL\SDL.h"
 #include "SDL\SDL_image.h"
+#include "Const.h"
 #include <stdio.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
 
-const int PADDLE_WIDTH = 128;
-const int PADDLE_HEIGHT = 32;
+int brickSide, wallSide, livesCounter, levelCounter;
 
-const int BRICK_WIDTH = 80;
-const int BRICK_HEIGHT = 40;
-
-const float BALL_SPEED = 600;
-const float PADDLE_SPEED = 600;
-const int BALL_DIAM = 24;
-
-int state[MAX_MAP_Y*MAX_MAP_X] = { 0 };
-int iter, brickSide, wallSide;
 int paddlestick;
 
+time_t t;
 Uint32 frametimes[FRAME_VALUES];
 Uint32 frametimelast;
 Uint32 framecount;
@@ -35,26 +21,21 @@ float framespersecond;
 
 float delta;
 
-struct Paddle { float padPosX, padPosY; };
-struct Paddle paddle;
-
-struct Ball { float ballPosX, ballPosY; float dirX, dirY; };
-struct Ball ball;
-
-struct Bricks { int tile[MAX_MAP_Y][MAX_MAP_X]; int state[MAX_MAP_Y*MAX_MAP_X]; };
-struct Bricks bricks = {0};
-
 int init();
 int loadMedia();
-void loadBricks(char* path);
+void loadLevel(char* path);
 void close();
 
-void renderBrick(SDL_Rect* brick, int x, int y);
+void renderBrick(SDL_Rect* brick, int x, int y, int tilePos);
 void renderBall();
 void renderPaddle();
+void renderLives(SDL_Rect* live,int x, int y);
+void drawLives();
 
 void moveBall(float delta);
 void movePaddle(float shift, float delta);
+
+void resetLevel();
 
 void changeDirection(float dirx, float diry);
 
@@ -62,19 +43,46 @@ int PadCollision();
 int WallCollision();
 int BrickCollision();
 
-//Loads individual image as texture
+char *bricksPaths[] =
+{
+	"Images/CyanBrick.png",
+	"Images/GreenBrick.png",
+	"Images/YellowBrick.png",
+	"Images/Purplebrick.png"
+};
+
+char *lelvelPaths[] =
+{
+	"Map/1.dat",
+	"Map/2.dat",
+	"Map/3.dat",
+	"Map/4.dat",
+	"Map/5.dat",
+};
+
+struct Paddle { float padPosX, padPosY; };
+struct Paddle paddle;
+
+struct Ball { float ballPosX, ballPosY; float dirX, dirY; };
+struct Ball ball;
+
+struct Bricks { int tile[MAX_MAP_Y][MAX_MAP_X]; int state[MAX_MAP_Y*MAX_MAP_X]; int color[MAX_MAP_Y*MAX_MAP_X]; };
+struct Bricks bricks;
 
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 
 SDL_Texture* loadTexture(char* path);
 SDL_Texture* gBackground = NULL;
+SDL_Texture* gWin = NULL;
 SDL_Texture* gPaddle = NULL;
 SDL_Texture* gBall = NULL;
-SDL_Texture* gBrick = NULL;
+SDL_Texture* gBricks[4] = { 0 };
+SDL_Texture* gLive = NULL;
 
 SDL_Rect padRect;
 SDL_Rect ballRect;
 SDL_Rect bricksRect[MAX_MAP_Y*MAX_MAP_X];
+SDL_Rect lives[9];
 
 #endif
